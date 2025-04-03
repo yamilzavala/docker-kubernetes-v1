d8b51d6b91f8cb769baa9410d99f3f99b5fb8465
yamilzavala <zavalayamil9@gmail.com>
2025-04-03 06:25:23 -0300

    storing notebookId added

--- 14.compose-project/notes-backend/src/routes.js
+++ 14.compose-project/notes-backend/src/routes.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import NotesModel from './models.js'
 import mongoose from 'mongoose';
+import axios from 'axios';
 
+const notebooksApiUrl = process.env.NOTEBOOKS_API_URL;
 const notesRouter = express.Router();
 
 //middlewares start
@@ -15,15 +17,42 @@ function validateId(req, res, next) {
 //Create a new notes: POST '/'
 notesRouter.post('/', async (req, res) => {    
     try {
-        const {title, content} = req.body;
+        const {title, content, notebookId} = req.body;
+        let validateNotebookId = null;
+        if(!notebookId) {
+            console.info({
+                message: 'Notebookid not provided. Stroring without notebook'
+            })
+        } else if(!mongoose.Types.ObjectId.isValid(notebookId)) {
+            return res.status(404).json({error: 'Notebook Not found', notebookId})
+        } else {
+            try {
+                await axios.get(`${notebooksApiUrl}/${notebookId}`)            
+            } catch (error) {
+                const jsonError = error.toJSON()
+                if(jsonError.status === 404) {
+                    return res.status(404).json({error: 'Notebook Not found', notebookId})
+                } else {
+                    console.error({
+                        message: 'Error verifying notebookId!. Notebooks services not available. Storing note with id for later validation.',
+                        notebookId,
+                        error: error.message
+                    })
+                }
+            } finally {
+                validateNotebookId = notebookId;
+            }
+        }
+
         if(!title || !content) return res.status(400).json({error: 'title and content are required'})
-        const resp = new NotesModel({title, content})
+        const resp = new NotesModel({title, content, notebookId: validateNotebookId})
         await resp.save();
         return res.status(201).json({message: 'Successfull notes creation', data: resp})
     } catch (error) {
-        return res.status(500).json({message: 'Server error', error})
+        return res.status(500).json({error: 'Server error', error})
     }
 })
+
 
 
 //Retrieve all notes: GET '/'
@@ -32,7 +61,7 @@ notesRouter.get('/', async (req, res) => {
         const resp = await NotesModel.find({})
         return res.status(200).json({data: resp})
     } catch (error) {
-        return res.status(500).json({message: 'Server error', error})
+        return res.status(500).json({error: 'Server error', error})
     }
 })
 
@@ -43,7 +72,7 @@ notesRouter.get('/:id', validateId, async (req, res) => {
         if(!resp) return res.status(404).json({error: 'Id not found'})
         return res.status(200).json({data: resp})
     } catch (error) {
-        return res.status(500).json({message: 'Server error', error})
+        return res.status(500).json({error: 'Server error', error})
     }
 })
 
@@ -55,7 +84,7 @@ notesRouter.put('/:id', validateId, async (req, res) => {
         if(!resp) return res.status(404).json({error: 'Id not found'})
         return res.status(201).json({message: 'Successfull notes update', data: resp})
     } catch (error) {
-        return res.status(500).json({message: 'Server error', error})
+        return res.status(500).json({error: 'Server error', error})
     }
 })
 
@@ -67,7 +96,7 @@ notesRouter.delete('/:id', validateId, async (req, res) => {
         if(!resp) return res.status(404).json({error: 'Id not found'})
         return res.sendStatus(204);
     } catch (error) {
-        return res.status(500).json({message: 'Server error', error})
+        return res.status(500).json({error: 'Server error', error})
     }
 })
 
